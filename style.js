580d3e0732dce4f9161ffc9fc92bfb4a1754c661
Jahnavi Samala <jahnavisamala69@gmail.com>
2026-09-18 16:33:51 +0530

    style.js

---
+++ style.js
@@ -0,0 +1,12 @@
+.grid {
+    display: grid;
+    grid-template-columns: repeat(3, 100px);
+    gap: 10px;
+}
+
+.grid div {
+    background-color: lightblue;
+    padding: 30px;
+    text-align: center;
+    border: 1px solid black;
+}
